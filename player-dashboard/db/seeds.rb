7ba0fdf13106c8adf0b5b3de0ef89f2e5c14dfa8
benstew <benstewart2008@gmail.com>
2016-01-24 20:42:43 -0500

    Updated seed file

--- player-dashboard/db/seeds.rb
+++ player-dashboard/db/seeds.rb
@@ -1,13 +1,25 @@
 
 require 'pry'
 
+File.open('../../nba-api/nba-scraper/stats.json', 'r') do |file|
+  file.each do |line|
+  all_players = JSON.parse(line)
 
-    File.open('../../nba-api/nba-scraper/stats.json', 'r') do |file|
-     file.each do |line|
-      players = JSON.parse(line)
+    all_players.each do |index, player|
 
-      binding.pry
+      Player.create(name: player["name"],
+                    team: player["team"],
+                    games_played: player["games_played"],
+                    wins: player["wins"],
+                    losses: player["losses"],
+                    minutes: player["minutes"],
+                    points: player["points"],
+                    touches: player["touches"],
+                    time_of_possession: player["time_of_possession"],
+                    seconds_per_touch: player["seconds_per_touch"],
+                    dribbles_per_touch: player["dribbles_per_touch"],
+                    points_per_touch: player["points_per_touch"])
 
-       Player.create(name: "Ben")
-     end
-   end
+    end
+  end
+end
