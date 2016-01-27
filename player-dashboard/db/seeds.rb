0715a614939743f3a04121a6efcf3a67fce917e3
benstew <benstewart2008@gmail.com>
2016-01-27 15:36:39 -0500

    Updated table, seed file, and view to account for front court touches

--- player-dashboard/db/seeds.rb
+++ player-dashboard/db/seeds.rb
@@ -6,6 +6,12 @@ File.open('../../nba-api/nba-scraper/json/sportvu_stats.json', 'r') do |file|
 
     all_players.each do |index, player|
 
+      points = (player["points"])
+      frontcourt_touches = player["frontcourt_touches"]
+
+      fc_points_calc = (points/frontcourt_touches).round(4)
+
+
       Player.create(name: player["name"],
                     # position:
                     team: player["team"],
@@ -19,7 +25,16 @@ File.open('../../nba-api/nba-scraper/json/sportvu_stats.json', 'r') do |file|
                     seconds_per_touch: player["seconds_per_touch"],
                     dribbles_per_touch: player["dribbles_per_touch"],
                     points_per_touch: player["points_per_touch"],
-                    position: player["position"])
+                    position: player["position"],
+                    frontcourt_touches: player["frontcourt_touches"],
+                    elbow_touches: player["elbow_touches"],
+                    post_touches: player["post_touches"],
+                    paint_touches: player["paint_touches"],
+                    points_per_elbow_touch: player["points_per_elbow_touch"],
+                    points_per_post_touch: player["points_per_post_touch"],
+                    points_per_paint_touch: player["points_per_paint_touch"],
+                    points_per_fc_touch: fc_points_calc
+                    )
 
     end
   end
