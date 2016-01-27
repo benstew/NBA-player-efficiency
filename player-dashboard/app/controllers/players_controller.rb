78ce1c26969427c2d25a8316034674becc742971
benstew <benstewart2008@gmail.com>
2016-01-26 19:41:31 -0500

    Added pagination to player index page

--- player-dashboard/app/controllers/players_controller.rb
+++ player-dashboard/app/controllers/players_controller.rb
@@ -1,10 +1,12 @@
 class PlayersController < ApplicationController
+  helper_method :sort_column, :sort_direction
   before_action :set_player, only: [:show, :edit, :update, :destroy]
 
   # GET /players
   # GET /players.json
   def index
-    @players = Player.all
+    # @players = Player.all
+    @players = Player.order(sort_column + " " + sort_direction).paginate(:per_page => 5, :page => params[:page])
   end
 
   # GET /players/1
@@ -70,5 +72,13 @@ class PlayersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def player_params
       params.require(:player).permit(:name, :team, :games_played, :wins, :losses, :minutes, :points, :touches, :time_of_possession, :seconds_per_touch, :dribbles_per_touch, :points_per_touch)
+    end
+
+    def sort_column
+      Player.column_names.include?(params[:sort]) ? params[:sort] : "name"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
     end
 end
