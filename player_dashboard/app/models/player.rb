ce1a5d60c5e73822f281e5fff171e84c62b8817c
benstew <benstewart2008@gmail.com>
2016-01-28 09:53:43 -0500

    Updated views to included analysis descriptions, updated index page header, mini refactorings

--- player_dashboard/app/models/player.rb
+++ player_dashboard/app/models/player.rb
@@ -2,30 +2,30 @@
 #
 # Table name: players
 #
-#  id                           :integer          not null, primary key
-#  name                         :string
-#  team                         :string
-#  games_played                 :integer
-#  wins                         :integer
-#  losses                       :integer
-#  minutes                      :decimal(, )
-#  points                       :decimal(, )
-#  touches                      :decimal(, )
-#  time_of_possession           :decimal(, )
-#  seconds_per_touch            :decimal(, )
-#  dribbles_per_touch           :decimal(, )
-#  points_per_touch             :decimal(, )
-#  created_at                   :datetime         not null
-#  updated_at                   :datetime         not null
-#  position                     :string
-#  front_court_touches          :float
-#  elbow_touches                :float
-#  post_touches                 :float
-#  paint_touches                :float
-#  points_per_elbow_touch       :float
-#  points_per_post_touch        :float
-#  points_per_paint_touch       :float
-#  points_per_front_court_touch :float
+#  id                     :integer          not null, primary key
+#  name                   :string
+#  team                   :string
+#  games_played           :integer
+#  wins                   :integer
+#  losses                 :integer
+#  minutes                :decimal(, )
+#  points                 :decimal(, )
+#  touches                :decimal(, )
+#  time_of_possession     :decimal(, )
+#  seconds_per_touch      :decimal(, )
+#  dribbles_per_touch     :decimal(, )
+#  points_per_touch       :decimal(, )
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  position               :string
+#  frontcourt_touches     :decimal(, )
+#  elbow_touches          :decimal(, )
+#  post_touches           :decimal(, )
+#  paint_touches          :decimal(, )
+#  points_per_elbow_touch :decimal(, )
+#  points_per_post_touch  :decimal(, )
+#  points_per_paint_touch :decimal(, )
+#  points_per_fc_touch    :float
 #
 
 class Player < ActiveRecord::Base
@@ -44,23 +44,23 @@ class Player < ActiveRecord::Base
   end
 
   def self.average_points
-    (average(:points)).round(3)
+    (average(:points)).round(2)
   end
 
   def self.average_minutes
-    (average(:minutes)).round(3)
+    (average(:minutes)).round(2)
   end
 
   def self.average_time_of_possession
-    (average(:time_of_possession)).round(3)
+    (average(:time_of_possession)).round(2)
   end
 
   def self.average_seconds_per_touch
-    (average(:seconds_per_touch)).round(3)
+    (average(:seconds_per_touch)).round(2)
   end
 
   def self.average_dribbles_per_touch
-    (average(:dribbles_per_touch)).round(3)
+    (average(:dribbles_per_touch)).round(2)
   end
 
   def self.average_points_per_touch
@@ -68,15 +68,15 @@ class Player < ActiveRecord::Base
   end
 
   def self.average_elbow_touches
-    (average(:elbow_touches)).round(3)
+    (average(:elbow_touches)).round(2)
   end
 
   def self.average_post_touches
-    (average(:post_touches)).round(3)
+    (average(:post_touches)).round(2)
   end
 
   def self.average_paint_touches
-    (average(:paint_touches)).round(3)
+    (average(:paint_touches)).round(2)
   end
 
   def self.average_points_per_elbow_touch
--- player_dashboard/test/models/player_test.rb
+++ player_dashboard/test/models/player_test.rb
@@ -2,30 +2,30 @@
 #
 # Table name: players
 #
-#  id                           :integer          not null, primary key
-#  name                         :string
-#  team                         :string
-#  games_played                 :integer
-#  wins                         :integer
-#  losses                       :integer
-#  minutes                      :decimal(, )
-#  points                       :decimal(, )
-#  touches                      :decimal(, )
-#  time_of_possession           :decimal(, )
-#  seconds_per_touch            :decimal(, )
-#  dribbles_per_touch           :decimal(, )
-#  points_per_touch             :decimal(, )
-#  created_at                   :datetime         not null
-#  updated_at                   :datetime         not null
-#  position                     :string
-#  front_court_touches          :float
-#  elbow_touches                :float
-#  post_touches                 :float
-#  paint_touches                :float
-#  points_per_elbow_touch       :float
-#  points_per_post_touch        :float
-#  points_per_paint_touch       :float
-#  points_per_front_court_touch :float
+#  id                     :integer          not null, primary key
+#  name                   :string
+#  team                   :string
+#  games_played           :integer
+#  wins                   :integer
+#  losses                 :integer
+#  minutes                :decimal(, )
+#  points                 :decimal(, )
+#  touches                :decimal(, )
+#  time_of_possession     :decimal(, )
+#  seconds_per_touch      :decimal(, )
+#  dribbles_per_touch     :decimal(, )
+#  points_per_touch       :decimal(, )
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  position               :string
+#  frontcourt_touches     :decimal(, )
+#  elbow_touches          :decimal(, )
+#  post_touches           :decimal(, )
+#  paint_touches          :decimal(, )
+#  points_per_elbow_touch :decimal(, )
+#  points_per_post_touch  :decimal(, )
+#  points_per_paint_touch :decimal(, )
+#  points_per_fc_touch    :float
 #
 
 require 'test_helper'
